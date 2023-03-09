96a2201e3e15e5300ce234a79821314484cd5496
jcpresents <jaduke89@gmail.com>
2023-03-09 03:01:29 -0500

    Initial commit

---
+++ archive/Draft 5 (Public)/scripts.js
@@ -0,0 +1,35 @@
+// Collapse function for mobile rows //
+
+function collapseM() {
+  let x = document.getElementById("rowM")
+  x.classList.toggle("collapse");
+}
+
+function collapseI() {
+  let x = document.getElementById("rowI")
+  x.classList.toggle("collapse");
+}
+
+function collapseN() {
+  let x = document.getElementById("rowN")
+  x.classList.toggle("collapse");
+}
+
+function collapseG() {
+  let x = document.getElementById("rowG")
+  x.classList.toggle("collapse");
+}
+
+function collapseO() {
+  let x = document.getElementById("rowO")
+  x.classList.toggle("collapse");
+}
+
+// Modal Replacer Function //
+// function replaceModal() {
+//   const main = document.getElementById('swapper');
+//   const original = document.getElementByid('description1');
+//   const clone = original.cloneNode(true);
+  
+//   main.replaceWith(clone);
+// }
